3af860f1433dddfee9b4f0c2af76ba9471456151
NicholasKung <Nickung1212@gmail.com>
2020-02-10 14:26:39 -0500

    Added first and last name parameters to factory bot

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -2,6 +2,8 @@ require 'factory_bot'
 
 FactoryBot.define do
   factory :user do
+    first_name {'Nick'}
+    last_name {'Kung'}
     sequence(:email) {|n| "user#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
