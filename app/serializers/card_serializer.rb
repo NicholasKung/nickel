af49aee30b47f22a60547d62ae8c4b4e1e0d552a
NicholasKung <Nickung1212@gmail.com>
2020-02-10 17:34:31 -0500

    Added a card serializer

---
+++ app/serializers/card_serializer.rb
@@ -0,0 +1,9 @@
+class CardSerializer < ActiveModel::Serializer
+  attributes :id, :number, :limit, :fee, :name, :description, :date, :supplier, :image, :user, :full_name
+
+  belongs_to :user
+
+  def full_name
+    "#{object.user.first_name} #{object.user.last_name}"
+  end
+end
