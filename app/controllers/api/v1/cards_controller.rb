14b4cb771287921be46ad6fa9865af579427745b
NicholasKung <Nickung1212@gmail.com>
2020-02-11 15:26:50 -0500

    Added a create method in cards controller

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::CardsController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :show]
+  before_action :authenticate_user!, only: [:index, :show, :create]
   protect_from_forgery unless: -> { request.format.json? }
 
   def index
@@ -9,5 +9,23 @@ class Api::V1::CardsController < ApplicationController
 
   def show
     render json: Card.find(params["id"])
+  end
+
+  def create
+    card = Card.new(card_params)
+    card.user = current_user
+    if card.save
+      render json: { card: card }
+    else
+      render json: { error: card.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+
+
+  private
+
+  def card_params
+    params.permit(:number, :limit, :fee, :name, :description, :date, :supplier, :image)
   end
 end
