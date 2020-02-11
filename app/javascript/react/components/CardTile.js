be60d4774f97c8136703a1e8d1bc2f7fe250bf76
NicholasKung <Nickung1212@gmail.com>
2020-02-10 21:03:49 -0500

    Converted credit card image into link

--- app/javascript/react/components/CardTile.js
+++ app/javascript/react/components/CardTile.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+
 
 const CardTile = (props) => {
 
   return(
     <div className = "card-tile">
       <h1>{props.cardData.name}</h1>
-      <img src = {props.cardData.image} />
-      <h3>{props.cardData.description}</h3>
-      <p>Expiration Date: {props.cardData.date} Limit: ${props.cardData.limit} Annual Fee: ${props.cardData.fee}</p>
+      <Link to={`/cards/${props.cardData.id}`}>
+        <img src={props.cardData.image}/>
+      </Link>
     </div>
   )
 }
