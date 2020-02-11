f1cc0f377d1e9f96e359a45c0d1753d007539bb6
NicholasKung <Nickung1212@gmail.com>
2020-02-11 10:52:01 -0500

    Added authentication priviledges to signed in users only

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::CardsController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :show]
   protect_from_forgery unless: -> { request.format.json? }
 
   def index
-    render json: Card.all
+    user_cards = current_user.cards
+    render json: user_cards
   end
 
   def show
