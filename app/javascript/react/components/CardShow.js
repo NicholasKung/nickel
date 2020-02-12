415fb678b336a01c44ef76f341ba5666a7b53c0e
NicholasKung <Nickung1212@gmail.com>
2020-02-11 20:35:32 -0500

    created a handle delete method

--- app/javascript/react/components/CardShow.js
+++ app/javascript/react/components/CardShow.js
@@ -2,12 +2,18 @@ import React from 'react'
 
 const CardShow = (props) => {
 
+  const handleDelete = (event) => {
+    event.preventDefault()
+    props.deleteCard(props.cardData.id)
+  }
+
   return(
     <div className = "card-tile">
       <h1>{props.cardData.name}</h1>
       <img src = {props.cardData.image} />
       <h3>{props.cardData.description}</h3>
       <p>Expiration Date: {props.cardData.date} Limit: ${props.cardData.limit} Annual Fee: ${props.cardData.fee}</p>
+      <input onClick={handleDelete} className="button" type="submit" value="Delete Card"/>
     </div>
   )
 }
