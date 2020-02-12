cdbef02a3e02f4a7fe47f18cab2a7e637e6ec39f
NicholasKung <Nickung1212@gmail.com>
2020-02-11 20:37:49 -0500

    Moved down the form

--- app/javascript/react/components/NewCardForm.js
+++ app/javascript/react/components/NewCardForm.js
@@ -38,100 +38,103 @@ const NewCardForm = (props) => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className = "row columns large-6">
+    <form onSubmit={handleSubmit}>
 
-      <label>
-        Credit Card Number:
-        <input
-          name="number"
-          id="number"
-          type="text"
-          value={newCard.number}
-          onChange={handleChange}
-          maxLength="4"
-        />
-      </label>
+      <div className="columns large-6">
+        <label>
+          Credit Card Number:
+          <input
+            name="number"
+            id="number"
+            type="text"
+            value={newCard.number}
+            onChange={handleChange}
+            maxLength="4"
+          />
+        </label>
 
-      <label>
-        Credit Card Limit:
-        <input
-          name="limit"
-          id="limit"
-          type="text"
-          value={newCard.limit}
-          onChange={handleChange}
-        />
-      </label>
+        <label>
+          Credit Card Limit:
+          <input
+            name="limit"
+            id="limit"
+            type="text"
+            value={newCard.limit}
+            onChange={handleChange}
+          />
+        </label>
 
-      <label>
-        Credit Card Annual Fee:
-        <input
-          name="fee"
-          id="fee"
-          type="text"
-          value={newCard.fee}
-          onChange={handleChange}
-        />
-      </label>
+        <label>
+          Credit Card Annual Fee:
+          <input
+            name="fee"
+            id="fee"
+            type="text"
+            value={newCard.fee}
+            onChange={handleChange}
+          />
+        </label>
 
-      <label>
-        Credit Card Name:
-        <input
-          name="name"
-          id="name"
-          type="text"
-          value={newCard.name}
-          onChange={handleChange}
-        />
-      </label>
+        <label>
+          Credit Card Name:
+          <input
+            name="name"
+            id="name"
+            type="text"
+            value={newCard.name}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
 
-      <label>
-        Credit Card Description:
-        <input
-          name="description"
-          id="description"
-          type="text"
-          value={newCard.description}
-          onChange={handleChange}
-          placeholder="Description"
-        />
-      </label>
+      <div className="columns large-6">
+        <label>
+          Credit Card Description:
+          <input
+            name="description"
+            id="description"
+            type="text"
+            value={newCard.description}
+            onChange={handleChange}
+            placeholder="Description"
+          />
+        </label>
 
-      <label>
-        Credit Card Expiration Date:
-        <input
-          name="date"
-          id="date"
-          type="tel"
-          value={newCard.date}
-          onChange={handleChange}
-          placeholder="MM/YY"
-        />
-      </label>
+        <label>
+          Credit Card Expiration Date:
+          <input
+            name="date"
+            id="date"
+            type="tel"
+            value={newCard.date}
+            onChange={handleChange}
+            placeholder="MM/YY"
+          />
+        </label>
 
-      <label>
-        Credit Card Supplier:
-        <input
-          name="supplier"
-          id="supplier"
-          type="text"
-          value={newCard.supplier}
-          onChange={handleChange}
-        />
-      </label>
+        <label>
+          Credit Card Supplier:
+          <input
+            name="supplier"
+            id="supplier"
+            type="text"
+            value={newCard.supplier}
+            onChange={handleChange}
+          />
+        </label>
 
-      <label>
-        Credit Card Image:
-        <input
-          name="image"
-          id="image"
-          type="text"
-          value={newCard.image}
-          onChange={handleChange}
-        />
-      </label>
-
-
+        <label>
+          Credit Card Image:
+          <input
+            name="image"
+            id="image"
+            type="text"
+            value={newCard.image}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+      
       <div>
         <input className="button" type="submit" value="Add New Card"/>
       </div>
