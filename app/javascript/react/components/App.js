693f77be3195e1d2c8ac3aff0caf6a84398e8cfb
NicholasKung <Nickung1212@gmail.com>
2020-02-12 11:59:22 -0500

    Added a footer component

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import CardIndex from './CardIndex.js'
 import CardShowContainer from './CardShowContainer'
 import NewCardContainer from './NewCardContainer'
+import WelcomePage from './WelcomePage'
 
 export const App = (props) => {
   return (
     <div>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={CardIndex}/>
+          <Route exact path="/" component={WelcomePage}/>
           <Route exact path="/cards" component={CardIndex}/>
           <Route exact path="/cards/new" component={NewCardContainer}/>
           <Route exact path="/cards/:id" component={CardShowContainer}/>
