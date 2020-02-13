af7a5090e8eee965fcaa8a935628b13f10797627
NicholasKung <Nickung1212@gmail.com>
2020-02-13 14:16:54 -0500

    Added association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :cards
+  has_many :transactions
 
   validates :first_name, presence: true
   validates :last_name, presence: true
