d300393b1b6cef98759e2163c59b390832e4d0ac
NicholasKung <Nickung1212@gmail.com>
2020-02-14 15:51:07 -0500

    added validation for phone for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :encrypted_password, presence: true, length: { minimum: 6 }
 
+  validates :phone, phone: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
