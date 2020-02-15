0e2574581a804e28f4fc67b9b32914f8d0ef3275
NicholasKung <Nickung1212@gmail.com>
2020-02-15 14:01:06 -0500

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :cards, only: [:index, :show, :create, :update, :destroy] do
-        resources :transactions, only: [:index, :create] do
+        resources :transactions, only: [:index, :create, :destroy] do
         end
       end
     end
