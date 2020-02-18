d0cb6adedcbabdc3eb367fb0253c8db20a503888
NicholasKung <Nickung1212@gmail.com>
2020-02-18 13:37:04 -0500

    Removed Footer

--- app/javascript/react/components/TransactionTile.js
+++ app/javascript/react/components/TransactionTile.js
@@ -2,17 +2,10 @@ import React from 'react'
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(2),
-    },
-  },
-}));
+
 
 const TransactionTile = (props) => {
 
-const classes = useStyles();
 
 const handleDelete = (event) => {
   event.preventDefault()
@@ -22,11 +15,17 @@ const handleDelete = (event) => {
   return(
     <div className = "transaction-tile">
       <div>
-        <h3>{props.transactionData.name} || {props.transactionData.category} || ${props.transactionData.amount}</h3>
+        <div>
+          <h3>
+          {props.transactionData.name} || {props.transactionData.category} || ${props.transactionData.amount}
+          </h3>
+          <div className = "button-delete">
+            <Button variant="contained" color="primary" type="submit" onClick={handleDelete}>
+              X
+            </Button>
+          </div>
+        </div>
         <p>Entered: {props.transactionData.transaction_time}</p>
-        <Button className = {classes.margin} variant="contained" color="secondary" type="submit" onClick={handleDelete}>
-          X
-        </Button>
       </div>
     </div>
   )
