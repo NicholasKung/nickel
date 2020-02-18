6ac1617643635c9978c99ea81f6251701ce40492
NicholasKung <Nickung1212@gmail.com>
2020-02-17 20:44:33 -0500

    styling

--- app/javascript/react/components/CardTile.js
+++ app/javascript/react/components/CardTile.js
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom"
 const CardTile = (props) => {
 
   return(
-    <div className = "card-tile">
+    <div className = "card-tile-index">
       <h1>{props.cardData.name}</h1>
       <Link to={`/cards/${props.cardData.id}`}>
-        <img src={props.cardData.image}/>
+        <img className = "img-credit" src={props.cardData.image}/>
       </Link>
       <h3>Added on: {props.cardData.card_time}</h3>
     </div>
