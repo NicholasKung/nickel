171a11b5d96ad0df9390676a686127be5bed8460
NicholasKung <Nickung1212@gmail.com>
2020-02-18 20:40:10 -0500

    Added Twilio

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,4 +1,4 @@
-require "#{Rails.root}/app/services/twilio_client.rb"
+require_relative "../../../../app/services/twilio_client.rb"
 
 class Api::V1::TransactionsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
@@ -11,6 +11,7 @@ class Api::V1::TransactionsController < ApplicationController
     transaction.card = card
 
     if transaction.save
+      TwilioClient.new.send_text(current_user, "You have successfully created a new transaction")
       render json: transaction
     else
       render json: { error: transaction.errors.full_messages }, status: :unprocessable_entity
@@ -21,6 +22,7 @@ class Api::V1::TransactionsController < ApplicationController
     transaction = Transaction.find(params[:id])
 
     if current_user == transaction.user
+      transaction.destroy
       render json: { message: "Delete Successful." }
     else
       render json: { message: "Could not delete." }
