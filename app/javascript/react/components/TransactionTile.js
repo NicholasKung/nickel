c4816ab3b444521c8a5d31f3c52ab44b96c1ae5b
NicholasKung <Nickung1212@gmail.com>
2020-02-20 21:27:19 -0500

    Changed look of transaction tile

--- app/javascript/react/components/TransactionTile.js
+++ app/javascript/react/components/TransactionTile.js
@@ -16,9 +16,9 @@ const handleDelete = (event) => {
     <div className = "columns medium-4">
       <div className = "transaction-tile">
         <div>
-          <h3>
-          {props.transactionData.name} || {props.transactionData.category} || ${props.transactionData.amount}
-          </h3>
+          <h3>{props.transactionData.name}</h3>
+          <h4>${props.transactionData.amount}</h4>
+          <h5>{props.transactionData.category}</h5>
           <div className = "button-delete">
             <Button variant="contained" color="primary" type="submit" onClick={handleDelete}>
               X
